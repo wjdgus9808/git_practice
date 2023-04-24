075ec89460c7b8f14d071e45766829c900c45a01
wjdgus9808 <dbswjdgus9808@gmail.com>
2023-04-24 21:35:59 +0900

    feat: Print fizz
    Condition: i is multiple of 3.
    Otherwise, print i itself.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,4 @@
-def do_fizzbuzz():
+def do_fizzbuzz(num:int):
 	"""
 	fizzbuzz: print fizz buzz fizzbuzz
 	3: fizz
@@ -7,6 +7,10 @@ def do_fizzbuzz():
 	etc:num
 	"""
 	print('hello')
-
+	for i in range(1,num+1):
+		if i%3==0:
+			print('fizz')
+		else:
+			print(f'{i}')
 if __name__=='__main__':
-	do_fizzbuzz()
+	do_fizzbuzz(16)
