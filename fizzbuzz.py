1d87f714c0f1d6e52de990ce769c13975317fa5c
wjdgus9808 <dbswjdgus9808@gmail.com>
2023-04-24 21:38:18 +0900

    feat: Print buzz
    Condition: i is multiple of 5.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -10,7 +10,10 @@ def do_fizzbuzz(num:int):
 	for i in range(1,num+1):
 		if i%3==0:
 			print('fizz')
+		elif i%5==0:
+			print('buzz')
 		else:
 			print(f'{i}')
+		
 if __name__=='__main__':
 	do_fizzbuzz(16)
